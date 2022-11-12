4a5f4020e43445ecc2af1ba3bd03cb01466dbbac
Bianca Miranda <miranda.bianca41@gmail.com>
2022-11-11 21:03:41 -0300

    Leitura do arquivo de entrada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        List<String> comandos = new LinkedList<>();
+        String linha;
+        while(input.hasNextLine()){
+            linha = input.nextLine();
+            comandos.add(linha);
+        };
+        comandos.forEach(dado -> System.out.println(dado));
+        input.close();
     }
 }
